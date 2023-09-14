178af271050780dbe0a88f3ea3b84f7c7556a8fa
h585911 <585911@stud.hvl.no>
2023-09-14 11:12:30 +0200

    First commit

---
+++ OBLIG1/src/no/hvl/dat100/Trinnskatt.java
@@ -0,0 +1,11 @@
+package no.hvl.dat100;
+
+public class Trinnskatt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello GitHub!");
+	}
+
+}
